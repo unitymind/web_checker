30a17321d756fdb64176d558039f986680ced86b
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2014-07-01 19:36:39 +0400

    [0.1.2] Move CLI to main module

--- lib/web_checker.rb
+++ lib/web_checker.rb
@@ -1,5 +1,80 @@
-require "web_checker/version"
+require 'web_checker/version'
+require 'thor'
 
 module WebChecker
+  class CLI < Thor
+    default_task :start
 
+    desc 'start', 'Start the web-checker service'
+
+    method_option :url,
+                  type: :string,
+                  required: true,
+                  banner:  'Url for monitoring'
+
+    method_option :email,
+                  type: :string,
+                  required: true,
+                  banner:  'Email for notifications'
+
+    method_option :delivery_method,
+                  type: :string,
+                  default: 'sendmail',
+                  banner:  'Delivery email method'
+
+    method_option :twilio,
+                  type: :boolean,
+                  default: false,
+                  banner:  'Use twilio for SMS notifications'
+
+    method_option :phone,
+                  type: :string,
+                  banner: 'Mobile phone number for SMS notifications'
+
+    method_option :twilio_sid,
+                  type: :string,
+                  banner: 'Twilio sid value'
+
+    method_option :twilio_token,
+                  type: :string,
+                  banner: 'Twilio token value'
+
+    method_option :twilio_from,
+                  type: :string,
+                  banner: 'Twilio from value'
+
+    method_option :threshold,
+                  type: :array,
+                  default: %w{5 10 50 100 500},
+                  banner: 'Attempts threshold for notifications'
+
+    method_option :refresh_rate,
+                  type: :numeric,
+                  default: 5,
+                  banner: 'Refresh rate for checking in seconds'
+
+    def start
+      trap("INT") { self.stop }
+
+      begin
+        @checker = WebChecker::Workflow.new(options)
+      rescue WebChecker::NotHttpURIError, WebChecker::InvalidHostError => e
+        puts "--url error. #{e.message}"
+        exit
+      end
+
+      EventMachine.run do
+        @checker.run
+      end
+    end
+
+    no_commands do
+      def stop
+        @checker.cancel
+        EventMachine.stop
+        exit
+      end
+    end
+
+  end
 end
